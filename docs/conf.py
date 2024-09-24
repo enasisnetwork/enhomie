9de550feaefe0c6e83fc9f701ea8d3b401a92462
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-09-24 03:32:46 +0000

    Update for docs script

--- docs/conf.py
+++ docs/conf.py
@@ -37,28 +37,9 @@ always_document_param_types = True
 intersphinx_mapping = {
     'encommon': ('https://encommon.readthedocs.io/en/latest', None),
     'enconnect': ('https://enconnect.readthedocs.io/en/latest', None),
+    'jinja2': ('https://jinja.palletsprojects.com/en/latest', None),
+    'netaddr': ('https://netaddr.readthedocs.io/en/latest', None),
     'pydantic': ('https://docs.pydantic.dev/latest', None),
     'pytest': ('https://docs.pytest.org/latest', None),
-    'python': ('https://docs.python.org/3', None)}
-
-nitpick_ignore = [
-
-    # Seems to be an issue using Pydantic
-    ('py:class', 'Field'),
-    ('py:class', 'FieldInfo'),
-    ('py:class', 'Ge'),
-    ('py:class', 'Le'),
-    ('py:class', 'MinLen'),
-    ('py:class', 'NoneType'),
-    ('py:class', '_PydanticGeneralMetadata'),
-
-    # Seems to be an issue using SQLAlchemy
-    ('py:class', 'MetaData'),
-    ('py:class', '_RegistryType'),
-    ('py:class', '_orm.registry'),
-    ('py:class', '_orm.Mapper'),
-    ('py:class', '_schema.MetaData'),
-    ('py:class', '_schema.Table'),
-
-    # Not sure what causes these warnings
-    ('py:class', 'respx.router.MockRouter')]
+    'python': ('https://docs.python.org/3', None),
+    'sqlalchemy': ('https://docs.sqlalchemy.org/en/20', None)}
